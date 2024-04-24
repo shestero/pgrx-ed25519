e5013c7af389507503669901f2823d4e8262cc6c
Michael Shestero <shestero@mail.ru>
2024-04-24 21:58:29 +0200

    Update lib.rs clean up

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use pgrx::*;
 use ed25519_dalek::{VerifyingKey, Verifier, SigningKey, Signature, Signer, SignatureError};
 use rand::rngs::OsRng;
 
-
 pg_module_magic!();
 
 #[pg_extern]
@@ -26,31 +25,3 @@ fn ed25519_verify(text: &[u8], sign: &[u8], pub_key: &[u8]) -> Result<bool, Sign
     Ok(ok)
 }
 
-// todo
-//use ed25519_dalek::pkcs8::DecodePublicKey;
-
-#[pg_extern]
-fn test() -> String {
-
-    /*
-    let pem = "-----BEGIN PUBLIC KEY-----
-MCowBQYDK2VwAyEAGb9ECWmEzf6FQbrBZ9w7lshQhqowtrbLDFw4rXAxZuE=
------END PUBLIC KEY-----";
-
-    let verifying_key = VerifyingKey::from_public_key_pem(pem)
-        .expect("invalid public key PEM");
-    */
-
-    let mut csprng = OsRng;
-    let signing_key: SigningKey = SigningKey::generate(&mut csprng);
-
-    let message: &[u8] = b"This is a test of the tsunami alert system.";
-    let signature: Signature = signing_key.sign(message);
-
-    let ok1 = signing_key.verify(message, &signature).is_ok();
-
-    let verifying_key: VerifyingKey = signing_key.verifying_key();
-    let ok2 = verifying_key.verify(message, &signature).is_ok();
-
-    format!("ed25519: ok1={ok1} ok2={ok2}")
-}
